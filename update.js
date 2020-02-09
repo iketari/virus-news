78a5b5ce4cad12ac9a8c33f6cd2b4002cf51aefc
Artsiom Mezin <iketari@gmail.com>
2020-02-09 20:24:06 +0000

    Get DynamoDB update from the git source

---
+++ update.js
@@ -0,0 +1,140 @@
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+const util = require('util');
+const fetch = require('node-fetch');
+
+const log = obj => console.log(util.inspect(obj, {showHidden: false, depth: null}));
+
+// Set the region 
+AWS.config.update({region: 'eu-west-1'});
+
+const TABLE = 'coronavirus-data';
+
+function getRemoteDataSrc(type) {
+  return `https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series/time_series_2019-ncov-${type}.csv`;
+}
+
+async function getRemoteData(url) {
+  try {
+    const response = await fetch(url);
+    const body = await response.text();
+    return body;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+function getCSVData(file) {
+  return fs.readFileSync(file);
+}
+
+function getParsedData(csvData) {
+  return parse(csvData, {
+    columns: true,
+    skip_empty_lines: true
+  });
+}
+
+/**
+ * Returns total numbers by date
+ * @returns {Object.<string, number>}
+ */
+function getTotalNumbers(locations) {
+  const metaFields = ['Province/State', 'Country/Region', 'First confirmed date in country', 'Lat', 'Long'];
+  return locations.reduce((acc, curr) => {
+    Object.keys(curr).forEach(key => {
+      if (metaFields.includes(key)) return;
+
+      let ts = Date.parse(key);
+      if (isNaN(ts)) return;
+      let tsKey = ts.toString();
+      let currValue = parseInt(curr[key], 10);
+
+      if (acc[tsKey] == null) {
+        acc[tsKey] = !isNaN(currValue) ? currValue : 0;
+        return;
+      }
+
+      acc[tsKey] =  acc[tsKey] != null && !isNaN(currValue) ? (acc[tsKey] + currValue) : acc[tsKey];
+    });
+
+    return acc;
+  }, {});
+}
+
+
+const fields = ['Confirmed', 'Recovered', 'Deaths'];
+
+async function processData(type, data){
+  const csvData = await getRemoteData(getRemoteDataSrc(type));
+  const parsedData = getParsedData(csvData);
+  const totalNumbers = getTotalNumbers(parsedData);
+
+  Object.entries(totalNumbers).forEach(([ts, value]) => {
+    data[ts] = {
+      ...(data[ts] || {}),
+      [type]: value
+    };
+  });
+
+  return data;
+}
+
+function cleanUp(data) {
+  for (let itemKey in data) {
+    const itemData = data[itemKey];
+    if (fields.every(fieldName => fieldName in itemData)) continue;
+
+    delete data[itemKey];
+  }
+}
+
+function upload(preparedData) {
+  const params = {
+    RequestItems: {}
+  };
+  
+  params.RequestItems[TABLE] = Object.entries(preparedData).map(([key, data]) => {
+      return {
+          PutRequest: {
+              Item: {
+                  'date': {N: '' + key},
+                  'Confirmed': {N: '' + data['Confirmed']},
+                  'Recovered': {N: '' + data['Recovered']},
+                  'Death': {N: '' + data['Deaths']}
+              }
+          }
+      }
+  })
+  .sort((a,b) => {
+    return b.PutRequest.Item.date.N - a.PutRequest.Item.date.N; // most fresh data
+  })
+  .slice(0, 25); // AWS limits it!
+
+  log(params.RequestItems[TABLE]);
+  
+  // Call DynamoDB to add the item to the table
+  ddb.batchWriteItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+}
+
+// Main logic
+async function main() {
+  let data = await processData('Confirmed', {});
+  await processData('Recovered', data);
+  await processData('Deaths', data);
+
+  cleanUp(data);
+  upload(data);
+}
+main();
